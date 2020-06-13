9c09bc3f5fa2429d9c5bf999ef1525a05b04b2c2
Alethar <benjamingao99@gmail.com>
2020-06-13 12:12:50 -0700

    Update Main.java

--- ResearchTool/Main.java
+++ ResearchTool/Main.java
@@ -4,4 +4,14 @@ public class Main
     public static void main(String[] args) {
         
     }
+    public void controller() throws InterruptedException {
+        GUI g = new GUI(this);
+        while(true) {
+            Thread.sleep(100);
+        }
+        
+    }
+    public void checkAction() {
+        
+    }
 }
