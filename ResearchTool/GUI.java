0c061ed89931bab9461b596de7614f5e9c726ec4
D0rkKnight <shouhanzen@gmail.com>
2020-06-13 15:51:57 -0700

    Further work on GUI

--- ResearchTool/GUI.java
+++ ResearchTool/GUI.java
@@ -9,16 +9,13 @@ public class GUI extends JFrame
 	private JPanel taskArea;
 	private JPanel mainArea;
 	
-	private JPanel dailyTasks;
-	private JPanel articleDropoff;
-	
     private JPanel searchbox;
     private JPanel searchtags;
     private JPanel searchpanels;
     
     public GUI(Searcher searcher) {
     	super("Researcher");
-	this.searcher = searcher;
+    	this.searcher = searcher;
     	setVisible(true);
     	setSize(1600, 900);
     	
@@ -43,7 +40,54 @@ public class GUI extends JFrame
     	
     	
     	//Sidebar
-    	taskArea.setLayout(new BoxLayout(taskArea, BoxLayout.Y_AXIS));
+    	taskArea.setLayout(new BorderLayout());
+    	JPanel taskAreaContent = new JPanel();
+    	taskAreaContent.setLayout(new BoxLayout(taskAreaContent, BoxLayout.Y_AXIS));
+    	taskArea.add(taskAreaContent, BorderLayout.NORTH);
     	
+    	//Logo
+    	JPanel logo = new JPanel();
+    	JLabel logoText = new JLabel("COVIDsource");
+    	logoText.setFont(new Font(logoText.getFont().getName(), Font.BOLD, 25));
+    	logo.add(logoText);
+    	taskAreaContent.add(logoText);
+    	
+    	//Daily Tasks
+    	JPanel dailyTasks = new JPanel(new GridBagLayout());
+    	//dailyTasks.setBackground(Color.red);
+    	addTask("Reclaim the holy lands: COMPLETE", dailyTasks);
+    	addTask("taskB", dailyTasks);
+    	addTask("taskC", dailyTasks);
+    	addTask("taskD", dailyTasks);
+    	addTask("taskE", dailyTasks);
+    	
+    	taskAreaContent.add(dailyTasks);
+    	
+    	//Article Dropoff
+    	JPanel articleDropoff = new JPanel();
+    	articleDropoff.setBackground(Color.green);
+    	taskAreaContent.add(articleDropoff);
+    }
+    
+    public void addTask(String taskText, JPanel container) {
+    	if (!(container.getLayout() instanceof GridBagConstraints)) {
+    		System.out.println("ERROR: Wrong layout type.");
+    	}
+    	
+    	int rows = container.getComponentCount()/2;
+    	
+    	JLabel task = new JLabel(taskText);
+    	GridBagConstraints taskGBC = new GridBagConstraints();
+    	taskGBC.gridx = 0;
+    	taskGBC.gridy = rows;
+    	taskGBC.weightx = 1;
+    	container.add(task, taskGBC);
+    	
+    	JCheckBox checkBox = new JCheckBox();
+    	GridBagConstraints checkBoxGBC = new GridBagConstraints();
+    	checkBoxGBC.gridx = 1;
+    	checkBoxGBC.gridy = rows;
+    	checkBoxGBC.weightx = 0.5;
+    	container.add(checkBox, checkBoxGBC);
     }
 }
