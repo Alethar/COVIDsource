e6228ec4d3a7aa4cea2b8e9c03f657a87b75ce9b
Alethar <benjamingao99@gmail.com>
2020-06-13 11:42:01 -0700

    Add files via upload

---
+++ ResearchTool/GUI.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class GUI extends JFrame
+{
+    private JPanel searchbox;
+    private JPanel searchtags;
+    private JPanel searchpanels;
+    
+}
