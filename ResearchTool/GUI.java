bd2c2074d2a96488905d1e678c944cd9b635b73d
D0rkKnight <shouhanzen@gmail.com>
2020-06-13 15:02:59 -0700

    Started on GUI

--- ResearchTool/GUI.java
+++ ResearchTool/GUI.java
@@ -1,13 +1,47 @@
 import java.awt.*;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 
 public class GUI extends JFrame
 {
+	private JPanel searchArea;
+	private JPanel taskArea;
+	private JPanel mainArea;
+	
+	private JPanel dailyTasks;
+	private JPanel articleDropoff;
+	
     private JPanel searchbox;
     private JPanel searchtags;
     private JPanel searchpanels;
     
+    public GUI() {
+    	super("Researcher");
+    	setVisible(true);
+    	setSize(1600, 900);
+    	
+    	mainArea = new JPanel();
+    	mainArea.setLayout(new BorderLayout());
+    	add(mainArea);
+    	
+    	taskArea = new JPanel();
+    	searchArea = new JPanel();
+    	
+    	searchArea.setBackground(Color.white);
+    	taskArea.setBackground(Color.gray);
+
+    	JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, searchArea, taskArea);
+    	splitPane.setEnabled(false);
+    	splitPane.setDividerSize(0);
+    	splitPane.setDividerLocation(getBounds().width - 350);
+    	//TODO: This needs to resize when the window size changes
+    	
+    	mainArea.add(splitPane, BorderLayout.CENTER);
+    	
+    	
+    	
+    	//Sidebar
+    	taskArea.setLayout(new BoxLayout(taskArea, BoxLayout.Y_AXIS));
+    	
+    }
 }
--- ResearchTool/Main.java
+++ ResearchTool/Main.java
@@ -2,6 +2,7 @@
 public class Main
 {
     public static void main(String[] args) {
-        
+        //Test test = new Test();
+    	GUI gui = new GUI();
     }
 }
