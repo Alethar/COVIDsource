b13a461c3e7ffc95959dbf9db61b9a1d67254ff3
Alethar <benjamingao99@gmail.com>
2020-06-13 19:58:16 -0700

    Update NewsArticle.java

--- ResearchTool/NewsArticle.java
+++ ResearchTool/NewsArticle.java
@@ -5,15 +5,42 @@ public class NewsArticle extends Article
 
     private String sampleText;
 
+    private String url;
+
     private int cred;
 
     private int bias;
 
 
-    public NewsArticle( String name, String sampleText, int cred, int bias )
+    public NewsArticle( String name, String sampleText, String url, int cred, int bias )
     {
-        super( name, sampleText );
+        super( name, sampleText, url );
         this.cred = cred;
         this.bias = bias;
+    }
+
+
+    /**
+     * 
+     * Accessor method for article credibility
+     * 
+     * @return
+     */
+    public int getCred()
+    {
+        return cred;
+
+    }
+
+
+    /**
+     * 
+     * Accessor method for article bias
+     * 
+     * @return
+     */
+    public int getBias()
+    {
+        return bias;
     }
 }
