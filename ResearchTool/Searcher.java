e2181c0949260eb60560b8a0da644b741551776e
Alethar <benjamingao99@gmail.com>
2020-06-14 15:04:20 -0700

    Update Searcher.java

--- ResearchTool/Searcher.java
+++ ResearchTool/Searcher.java
@@ -20,13 +20,19 @@ public class Searcher
 
     private ArrayList<Article> articles;
 
-    //private String[] newsNames = { "a", "b", "c" };
-    
-    private String[] newsURLs = { "www.achnews.org", "www.act.tv", "www.aftonbladet.se", "www.aldianews.com", "www.afj.org", "www.altnews.in", "www.alternet.org",  };
+    // private String[] newsNames = { "a", "b", "c" };
 
-    private int[] newsCred = { 3, 3, 2, 2, 4, 4, 2 }; //0 to 5, 0 is not credible and 5 is credible
+    private String[] newsURLs = {};
 
-    private double[] newsBias = { 1, 0.6, 1.1, 0.6, 1.1, 1.2, 0.3 }; //0 to 6, 0 is left and 6 is right
+    private int[] newsCred = {}; // 0 to 5, 0 is not
+                                 // credible and 5 is
+                                 // credible
+
+    private double[] newsBias = {}; // 0 to 6,
+                                    // 0 is
+                                    // left and
+                                    // 6 is
+                                    // right
 
 
     public Searcher()
@@ -59,19 +65,34 @@ public class Searcher
         for ( int i = 0; i < searchTerms.size(); i++ )
         {
             searchURL += searchTerms.get( i );
-            if ( i != searchTerms.size() )
+            if ( i != searchTerms.size() - 1 )
             {
                 searchURL += "+";
             }
         }
-        searchURL += "&nums=";
+        searchURL += "&num=";
         searchURL += ( "" + amount );
         try
         {
             Document webpage = Jsoup.connect( searchURL ).userAgent( "Mozilla/5.0" ).get();
+
             Elements results = webpage.select( "a.BVG0Nb" );
+            System.out.println( "Size: " + results.size() );
+
+            results.addAll( webpage.select( "div.ZINbbc.xpd.O9g5cc.uUPGi" ) );
+            Elements results2 = webpage.select( "div[class=ZINbbc.xpd.O9g5cc.uUPGi]" );
+            // ZINbbc xpd O9g5cc uUPGi
+
+            System.out.println( "Size: " + results2.size() );
+
+            // System.out.println("second results length: " + results.size());
+            PrintWriter out = new PrintWriter( new BufferedWriter( new FileWriter( "test.out" ) ) );
+            out.print( webpage.toString() );
+            out.println();
+            out.close();
             for ( Element result : results )
             {
+                System.out.println( result.toString() );
                 String author;
                 String linkHref;
                 String title;
@@ -80,24 +101,64 @@ public class Searcher
                  * normal link (like a twitter link or suggestion/advertisement
                  * from google) and skip it
                  */
-                try
+
+                if ( result.hasAttr( "href" ) )
                 {
                     linkHref = result.attr( "href" );
-                    String linkText = result.text();
-                    author = result.select( "div.BNeawe.tAd8D.AP7Wnd" ).first().text();
+                }
+                else
+                {
+                    System.out.println( "triggered" );
+                    linkHref = result.select( "a" ).first().attr( "href" );
+                    System.out.println( linkHref );
+                }
+                // if(result.select("span") == null ||
+                // result.select("span").first() == null ){
+                Elements my_r = result.select( "span" );
+                // title = result.select( "span" ).first().text();
+                if ( my_r.size() == 0 )
+                {
+                    my_r = result.select( "div.BNeawe.vvjwJb.AP7Wnd" );
+                    if ( my_r.size() == 0 )
+                    {
+                        // System.out.println("Allequal0");
+                        // System.out.println(result.toString());
+                        continue;
+                    }
+                }
+                title = my_r.first().text();
 
-                    //PrintWriter out = new PrintWriter(
-                    //    new BufferedWriter( new FileWriter( "test.out" ) ) );
-                    //out.print( result.toString() );
-                    //out.println();
-                    //out.close();
-                    title = result.select( "span" ).first().text();
+                System.out.println( "middle" );
+                try
+                {
+                    if ( result.select( "div.BNeawe.tAd8D.AP7Wnd" ).size() != 0 )
+                    {
+                        author = result.select( "div.BNeawe.tAd8D.AP7Wnd" ).first().text();
+                    }
+                    else
+                    {
+                        author = result.select( "div.BNeawe.UPmit.AP7Wnd" ).first().text();
+                    }
+                    System.out.println( "after" );
+
+                    // PrintWriter out = new PrintWriter(
+                    // new BufferedWriter( new FileWriter( "test.out" ) ) );
+                    // out.print( result.toString() );
+                    // out.println();
+                    // out.close();
 
                 }
                 catch ( NullPointerException noooo )
                 {
+
+                    System.out.println( "skipped" );
+
                     continue;
                 }
+                System.out.println( result.toString() );
+                System.out.println( linkHref );
+                System.out.println( searchURL );
+
                 String link = linkHref.substring( 6, linkHref.indexOf( "&" ) );
                 String smalllink = link.split( "/" )[2];
                 int newsID = isNews( smalllink );
@@ -127,31 +188,35 @@ public class Searcher
                 }
                 // System.out.println( "Text::" + linkText + ", URL::" + link);
             }
+            System.out.println( webpage.select( "div[class=kCrYT]" ).size() );
         }
-        catch ( IOException e )
+        catch ( Exception e )
         {
-            System.out.println( e );
+            System.out.println( "LARGE ERROR" );
             return e;
         }
-
+        System.out.println( searchURL );
+        System.out.println( articles.size() );
         return null;
         // String searchURL = GOOGLE_SEARCH_URL + "?q="+searchTerm+"&num="+num;
         // without proper User-Agent, we will get 403 error
         // Document doc =
         // Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
     }
-    
+
+
     /**
      * Clears source cache
      */
-    public void clearSources() {
-    	System.out.println("Sources cleared");
-    	articles = new ArrayList<>();
+    public void clearSources()
+    {
+        System.out.println( "Sources cleared" );
+        articles = new ArrayList<>();
     }
 
+
     /**
-     * 
-     * Returns an article from articles. If article num exceeds the length of
+     * 3 Returns an article from articles. If article num exceeds the length of
      * the list, null is returned
      * 
      * @param articleNum
@@ -199,46 +264,59 @@ public class Searcher
 
         return -1;
     }
-    
-    
-    /**
-     * Code ripped from https://mkyong.com/java/open-browser-in-java-windows-or-linux/
-     * @param url: url of the tab to open
-     */
-    static public void openBrowserTab(String url) {
-    	String os = System.getProperty("os.name").toLowerCase();
-    	Runtime rt = Runtime.getRuntime();
-    	
-    	String urlTrimmed = url.substring(1);
-    	
-    	try {
-    		if (os.indexOf("win") >= 0) {
-    			
-    			//Doesn't support urls like page.html#nameLink, maybe just delete those?
-    			rt.exec("rundll32 url.dll,FileProtocolHandler " + urlTrimmed);
-    		} 
-    		else if (os.indexOf("mac") >= 0) {
-    			rt.exec ("open " + urlTrimmed);
-    		} 
-    		else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
-    			//Best guess for unix platforms, when it comes to browsers
-    			String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
-  			             "netscape","opera","links","lynx"};
-    			
-    			//Build a command
-    			StringBuffer cmd = new StringBuffer();
-    	        for (int i=0; i<browsers.length; i++)
-    	            cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + urlTrimmed + "\" ");
 
-    	        rt.exec(new String[] { "sh", "-c", cmd.toString() });
-    		} else {
-    			System.out.println("Something is horribly borked");
-    			return;
-    		}
-    	}
-    	catch (Exception e){
-    		System.out.println("bork");
-    		return;
-    	}
+
+    /**
+     * Code ripped from
+     * https://mkyong.com/java/open-browser-in-java-windows-or-linux/
+     * 
+     * @param url:
+     *            url of the tab to open
+     */
+    static public void openBrowserTab( String url )
+    {
+        String os = System.getProperty( "os.name" ).toLowerCase();
+        Runtime rt = Runtime.getRuntime();
+
+        String urlTrimmed = url.substring( 1 );
+
+        try
+        {
+            if ( os.indexOf( "win" ) >= 0 )
+            {
+
+                // Doesn't support urls like page.html#nameLink, maybe just
+                // delete those?
+                rt.exec( "rundll32 url.dll,FileProtocolHandler " + urlTrimmed );
+            }
+            else if ( os.indexOf( "mac" ) >= 0 )
+            {
+                rt.exec( "open " + urlTrimmed );
+            }
+            else if ( os.indexOf( "nix" ) >= 0 || os.indexOf( "nux" ) >= 0 )
+            {
+                // Best guess for unix platforms, when it comes to browsers
+                String[] browsers = { "epiphany", "firefox", "mozilla", "konqueror", "netscape",
+                    "opera", "links", "lynx" };
+
+                // Build a command
+                StringBuffer cmd = new StringBuffer();
+                for ( int i = 0; i < browsers.length; i++ )
+                    cmd.append(
+                        ( i == 0 ? "" : " || " ) + browsers[i] + " \"" + urlTrimmed + "\" " );
+
+                rt.exec( new String[] { "sh", "-c", cmd.toString() } );
+            }
+            else
+            {
+                System.out.println( "Something is horribly borked" );
+                return;
+            }
+        }
+        catch ( Exception e )
+        {
+            System.out.println( "bork" );
+            return;
+        }
     }
 }
