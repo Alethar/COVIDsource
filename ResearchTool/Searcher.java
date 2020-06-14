a2ea2ef979d8e9d04768a46ad5744e6afaded5c1
Alethar <benjamingao99@gmail.com>
2020-06-13 18:22:21 -0700

    Update Searcher.java

--- ResearchTool/Searcher.java
+++ ResearchTool/Searcher.java
@@ -6,26 +6,61 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+
 public class Searcher
 {
     private static final String GOOGLE_SEARCH_URL = "https://www.google.com/search?q=";
-    //http://www.google.com/search?q=abc+def searches for "abc def"
+
+    // http://www.google.com/search?q=abc+def searches for "abc def"
     private GUI gui;
+
     private Article[] articles;
-    private String[] newsNames = {"a", "b", "c"};
-    private double[] newsCred = {4.5, 5, 4};
-    public Searcher() {
-        
+
+    private String[] newsNames = { "a", "b", "c" };
+
+    private double[] newsCred = { 4.5, 5, 4 };
+
+
+    public Searcher()
+    {
+
     }
-    public void setGUI(GUI gui) {
+
+
+    public void setGUI( GUI gui )
+    {
         this.gui = gui;
     }
+
+
     /**
      * 
      * Gets the required amount of sources and loads info into class variable
-     * @param amount of sources you want
+     * 
+     * @param amount
+     *            of sources you want
      */
-    public void getSources(String searchterm, int amount) {
-        
+    public void getSources( String searchterm, int amount )
+    {
+
+    }
+
+
+    /**
+     * 
+     * Returns an article from articles. If article num exceeds the length of
+     * the list, null is returned
+     * 
+     * @param articleNum
+     * @return
+     */
+    public Article getArticle( int articleNum )
+    {
+        if ( articleNum >= articles.length )
+        {
+            return null;
+        }
+        return articles[articleNum];
     }
 }
